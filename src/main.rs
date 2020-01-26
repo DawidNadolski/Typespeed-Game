de28dabf470f9adb028a5253045dd8b79ab39997
Dawid Nadolski <dawidnadolski@icloud.com>
2020-01-26 22:15:56 +0100

    Fix blinking text  bug

--- src/main.rs
+++ src/main.rs
@@ -17,27 +17,24 @@ struct Word {
 }
 
 impl Word {
-    fn new(ctx: &mut Context, raw_text: &str) -> GameResult<Word> {
-        let font = graphics::Font::new(ctx, "/DejaVuSerif.ttf")?;
-        let word = graphics::Text::new((raw_text, font, 20.0));
+    fn new(context: &mut Context, raw_text: &str) -> GameResult<Word> {
+        let font = graphics::Font::new(context, "/DejaVuSerif.ttf")?;
+        let word = graphics::Text::new((raw_text, font, 50.0));
         let mut rng = rand::thread_rng();
-        let pos = rng.gen_range(0.0, 720.0);
-        println!("{}", pos);
+        let random_y_pos = rng.gen_range(0.0, 720.0);
 
         Ok(Word { 
             x_pos: 0.0, 
-            y_pos: pos,
+            y_pos: random_y_pos,
             text: word
         })
     }
 
-    fn draw(&mut self, ctx: &mut Context) -> GameResult<()>  {
-        graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
+    fn draw(&mut self, context: &mut Context) -> GameResult<()>  {
 
         let dest_point = cgmath::Point2::new(self.x_pos, self.y_pos);
         self.x_pos += 1.0;
-        graphics::draw(ctx, &self.text, (dest_point,))?;
-        //graphics::present(ctx)?;
+        graphics::draw(context, &self.text, (dest_point,))?;
         Ok(())
     }
 }
@@ -47,41 +44,44 @@ struct MainState {
 }
 
 impl MainState {
-    fn new(ctx: &mut Context) -> GameResult<MainState> {
+    fn new(context: &mut Context) -> GameResult<MainState> {
         let mut words_vec = Vec::new();
-        words_vec.push(Word::new(ctx, "Pyszczek").unwrap());
-        words_vec.push(Word::new(ctx, "Kaczorek").unwrap());
-        words_vec.push(Word::new(ctx, "Groszek").unwrap());
-        words_vec.push(Word::new(ctx, "Wiktoria").unwrap());
-        words_vec.push(Word::new(ctx, "Dawid").unwrap());
+        words_vec.push(Word::new(context, "Chuj").unwrap());
+        words_vec.push(Word::new(context, "Dupa").unwrap());
+        words_vec.push(Word::new(context, "Kurwa").unwrap());
+        words_vec.push(Word::new(context, "Cipa").unwrap());
+        words_vec.push(Word::new(context, "XD").unwrap());
 
         let s = MainState { words: words_vec };
 
         Ok(s)
     }
+
+    fn draw(&mut self, context: &mut Context) -> GameResult<()> {
+        graphics::clear(context, [0.1, 0.2, 0.3, 1.0].into());
+
+        for word in &mut self.words {
+            word.draw(context);
+        }  
+        
+        Ok(())
+    }
 }
 
 impl event::EventHandler for MainState {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+    fn update(&mut self, _context: &mut Context) -> GameResult {
         Ok(())
     }
 
-    fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        //graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
-
-        for word in &mut self.words {
-            word.draw(ctx);
-            //println!("{:?}", )
-        }  
+    fn draw(&mut self, context: &mut Context) -> GameResult {
         
-        graphics::present(ctx)?;
-
+        self.draw(context)?;
+        graphics::present(context)?;
         Ok(())
     }
 }
 
 pub fn main() -> GameResult {
-    // GGEZ will search for files in resources
     let resource_dir = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
         let mut path = path::PathBuf::from(manifest_dir);
         path.push("resources");
@@ -94,8 +94,8 @@ pub fn main() -> GameResult {
         .add_resource_path(resource_dir)
         .window_setup(ggez::conf::WindowSetup::default().title("Typespeed! Type as fast as you can!"))
         .window_mode(ggez::conf::WindowMode::default().dimensions(1280.0, 720.0));
-    let (ctx, event_loop) = &mut cb.build()?;
+    let (context, event_loop) = &mut cb.build()?;
 
-    let state = &mut MainState::new(ctx)?;
-    event::run(ctx, event_loop, state)
+    let state = &mut MainState::new(context)?;
+    event::run(context, event_loop, state)
 }
